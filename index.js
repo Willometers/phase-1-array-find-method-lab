88ebafae2204ab922d75611f6720a0d9ac896621
BillyMiles <williammiles201@gmail.com>
2021-05-02 20:35:00 -0400

    commit tree b95c9075d74f9cca51904b0f538024017f877b2d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
 
+//   function superbowlWin(game) {
+//     return game.result === `W`;
+//   }
+
+//   console.log(record.find(superbowlWin.));
+
+ 
